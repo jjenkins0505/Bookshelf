127bef4ae72dc6ccb9c9269ecd3204fe8c34e49d
jjenkins0505 <jenkinsjazmyne@gmail.com>
2017-05-03 14:55:15 -0400

    Initial Commit

---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name= "Category Name")]
+        public string Name { get; set; }
+
+
+        //ICollection is more efficient when setting up a one to many relationship code first. type of collection (cousin to a list)
+        public virtual ICollection<Book> Books { get; set; }
+
+
+
+    }
+}
